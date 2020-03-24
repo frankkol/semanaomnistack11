9a332123dc53c5c0582cc4edbd69e94a37286447
Frank Rodrigues de Oliveira <frankkol@hotmail.com>
2020-03-24 18:57:29 -0300

    Correção Back-end

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,7 +4,7 @@ const routes = require('./routes')
 
 const app = express()
 
-app.use(cors()) //Import de camada de proteção
+app.use(cors()) //Import de camada de proteção com cors
 app.use(express.json()) //Necessario para que as rotas interpretem request do tipo json
 app.use(routes) //Import do arquivo de rotas para o Express
 
