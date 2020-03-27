2ef0166816e1dd14066dd750e652695734eb253c
Frank Rodrigues de Oliveira <frankkol@hotmail.com>
2020-03-27 19:13:04 -0300

    Validação Back-end  com Celebrate e Tests com Jest

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const { errors } = require('celebrate')
 const routes = require('./routes')
 
 const app = express()
@@ -7,5 +8,6 @@ const app = express()
 app.use(cors()) //Import de camada de proteção com cors
 app.use(express.json()) //Necessario para que as rotas interpretem request do tipo json
 app.use(routes) //Import do arquivo de rotas para o Express
+app.use(errors()) //Tratar erros de validação
 
-app.listen(3333)
+module.exports = app
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { celebrate, Segments, Joi } = require('celebrate')
 const OngController = require('./controllers/OngController')
 const IncidentController = require('./controllers/IncidentController')
 const ProfileController = require('./controllers/ProfileController')
@@ -6,15 +7,55 @@ const SessionControler = require('./controllers/SessionControler')
 
 const routes = express.Router()
 
-routes.post('/sessions', SessionControler.create)
+//Rotas para sessions
+routes.post('/sessions', celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        id: Joi.string().required().length(8)
+    })
+}), SessionControler.create)
 
+//Rotas para ongs
 routes.get('/ongs', OngController.index)
-routes.post('/ongs', OngController.create)
 
-routes.get('/incidents', IncidentController.index)
-routes.post('/incidents', IncidentController.create)
-routes.delete('/incidents/:id', IncidentController.delete)
+routes.post('/ongs', celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required().email(),
+        whatsapp: Joi.string().required().min(10).max(11),
+        city: Joi.string().required(),
+        uf: Joi.string().required().length(2)
+    })
+}), OngController.create)
 
-routes.get('/profile', ProfileController.index)
+//Rotas para incidents
+routes.get('/incidents', celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+        page: Joi.number()
+    })
+}), IncidentController.index)
+
+routes.post('/incidents', celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        title: Joi.string().required(),
+        description: Joi.string().required(),
+        value: Joi.number().required()
+    }),
+    [Segments.HEADERS]: Joi.object({
+        authorization: Joi.string().required()
+    }).unknown()
+}), IncidentController.create)
+
+routes.delete('/incidents/:id', celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+        id: Joi.number().required()
+    })
+}), IncidentController.delete)
+
+//Rotas para profile
+routes.get('/profile', celebrate({
+    [Segments.HEADERS]: Joi.object({
+        authorization: Joi.string().required()
+    }).unknown()
+}), ProfileController.index)
 
 module.exports = routes
